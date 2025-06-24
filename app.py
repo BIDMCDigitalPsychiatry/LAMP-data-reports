f690d07c8f8dcdde9b0c164f87672abf645e24da
Matthew Flathers <matthewflathers@matthews-air.bidmc.harvard.edu>
2025-06-24 12:19:32 -0400

    Refactor cleanup

--- app.py
+++ app.py
@@ -27,9 +27,7 @@ from flask_login import (
     login_required,
     logout_user,
     current_user,
-    UserMixin,
 )
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf.csrf import CSRFProtect
 import click
 
